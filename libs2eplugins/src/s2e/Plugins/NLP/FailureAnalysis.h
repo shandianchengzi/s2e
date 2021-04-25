a511e2b9fb2de7a5fbb2b9c5eeb36976f4e3dfe9
weizhou-chaojixx <weizhoulightning@gmail.com>
2021-04-25 15:19:43 +0800

    fixup! libs2eplugins:FailureAnalysis init commit

--- libs2eplugins/src/s2e/Plugins/NLP/FailureAnalysis.h
+++ libs2eplugins/src/s2e/Plugins/NLP/FailureAnalysis.h
@@ -23,14 +23,22 @@
 #ifndef S2E_PLUGINS_FailureAnalysis_H
 #define S2E_PLUGINS_FailureAnalysis_H
 
-#include <llvm/ADT/DenseMap.h>
+#include <deque>
+#include <inttypes.h>
 #include <s2e/CorePlugin.h>
 #include <s2e/Plugin.h>
+#include <s2e/Plugins/NLP/InvalidStatesDetection.h>
 #include <s2e/S2EExecutionState.h>
+#include <vector>
+
+#include <llvm/ADT/SmallVector.h>
 
 namespace s2e {
 namespace plugins {
 
+typedef std::pair<uint32_t /* peripheraladdress */, uint32_t /* pc */> UniquePeripheral;
+typedef std::map<uint64_t /* unique no */, uint32_t /* value */> NoMap;
+typedef std::map<UniquePeripheral, NoMap> AllSymbolicPeripheralRegsMap;
 class FailureAnalysis : public Plugin {
     S2E_PLUGIN
 public:
@@ -38,21 +46,24 @@ public:
     }
 
     void initialize();
-
-private:
-    typedef llvm::DenseMap<uint64_t, uint64_t> ForkCounts;
-    typedef std::map<std::string, ForkCounts> ModuleForkCounts;
-
-    ModuleForkCounts m_forkCount;
-
-    unsigned m_limit;
-    unsigned m_processForkDelay;
-
-    unsigned m_timerTicks;
-
-    void onStateForkDecide(S2EExecutionState *state, bool *doFork, const klee::ref<klee::Expr> &condition, bool *conditionFork);
+    void onExceptionExit(S2EExecutionState *state, uint32_t irq_no);
     void onFork(S2EExecutionState *state, const std::vector<S2EExecutionState *> &newStates,
                 const std::vector<klee::ref<klee::Expr>> &newConditions);
+    void onStateKill(S2EExecutionState *state);
+    void onStateSwitch(S2EExecutionState *current, S2EExecutionState *next);
+    void onStateForkDecide(S2EExecutionState *state, bool *doFork, const klee::ref<klee::Expr> &condition,
+                           bool *conditionFork);
+    void onInvalidStatesDetection(S2EExecutionState *state, uint32_t pc, InvalidStatesType type, uint64_t tb_num);
+
+private:
+    InvalidStatesDetection *onInvalidStateDectionConnection;
+    std::vector<S2EExecutionState *> irq_states;           // forking states in interrupt
+    int fs;                                                // count for false status fork states kill;
+    std::vector<S2EExecutionState *> false_type_phs_fork_states;
+
+    AllSymbolicPeripheralRegsMap getLastBranchTargetRegValues(S2EExecutionState *state, uint32_t irq_num);
+    bool getPeripheralExecutionState(std::string variablePeripheralName, uint32_t *phaddr, uint32_t *size,
+                                     uint32_t *pc, uint64_t *no);
 };
 
 } // namespace plugins
