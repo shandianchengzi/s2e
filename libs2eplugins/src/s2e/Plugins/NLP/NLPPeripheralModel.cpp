b7866c0bf9eff6278ec6f26d88080f05853e73f6
LanZhang <emma@LanZhangs-MacBook-Pro.local>
2021-04-25 08:56:25 -0400

    NLPPeripheralModel compile commit

--- libs2eplugins/src/s2e/Plugins/NLP/NLPPeripheralModel.cpp
+++ libs2eplugins/src/s2e/Plugins/NLP/NLPPeripheralModel.cpp
@@ -56,154 +56,6 @@ private:
     //};
     //0:= ; 1:>; 2: <; 3: >=; 4: <=
 
-    bool ReadMemofromFile(std::string fileName) {
-        std::ifstream fPHKB;
-        std::string line;
-        fPHKB.open(fileName, std::ios::in);
-        if (!fPHKB) {
-            getWarningsStream() << "Could not open cache peripheral knowledge base file: " 
-                                << fileName << " \n";
-            return false;
-        }
-
-        std::string peripheralcache;
-        while (getline(fPHKB, peripheralcache)) {
-            PeripheralReg reg;
-            if (getMemo(peripheralcache, &reg)) {
-                peripheral_regs_value_map[reg.phaddr] = reg;
-                if (reg.type == data_register_type)
-                    data_register = reg.phaddr;
-            }
-        }
-        return true;
-    }
-
-    bool ReadTAfromFile(std::string fileName) {
-        std::ifstream fPHKB;
-        std::string line;
-        fPHKB.open(fileName, std::ios::in);
-        if (!fPHKB) {
-            getWarningsStream() << "Could not open cache peripheral knowledge base file: " 
-                                << fileName << " \n";
-            return false;
-        }
-
-        std::string peripheralcache;
-        while (getline(fPHKB, peripheralcache)) {
-            std::vector<Equation> trigger;
-            std::vector<Equation> action;
-            if (getTApairs(peripheralcache, &trigger, &action)) {
-                allTAs.push_back(make_pair(trigger, action));
-            }
-        }
-        return true;
-    }
-
-    void SplitString(const std::string &s, std::vector<std::string> &v, const std::string &c) {
-        std::string::size_type pos1, pos2;
-        pos2 = s.find(c);
-        pos1 = 0;
-        while (std::string::npos != pos2) {
-            v.push_back(s.substr(pos1, pos2 - pos1));
-
-            pos1 = pos2 + c.size();
-            pos2 = s.find(c, pos1);
-        }
-        if (pos1 != s.length())
-            v.push_back(s.substr(pos1));
-    }
-
-    bool getMemo(std::string peripheralcache, PeripheralReg &reg) {
-        boost::smatch what;
-        if (!boost::regex_match(peripheralcache, what, MemoRegEx)) {
-            getWarningsStream() << "match false"
-                                << "\n";
-            exit(0);
-            return false;
-        }
-
-        if (what.size() != 1) {
-            getWarningsStream() << "wrong size = " << what.size() << "\n";
-            exit(0);
-            return false;
-        }
-
-        std::vector<std::string> v;
-        SplitString(what, v, "_");
-        reg.type = v[0];
-        reg.phaddr = std::stoull(v[1].c_str(), NULL, 10);
-        reg.reset = std::stoull(v[2].c_str(), NULL, 10);
-        reg.cur = reg.rest;
-        reg.t_size = 0;
-        reg.r_size = 0;
-        reg.t_value = 0;
-        reg.r_value = 0;
-        return true;
-    }
-
-    void getTApairs(std::string peripheralcache, std::vector<Equation> &trigger, std::vector<Equation> &action) {
-        std::vector<std::string> v;
-        SplitString(peripheralcache, v, ":");
-        std::string trigger = v[0];
-        std::string action = v[1];
-        bool trigger_rel = true, action_rel = true;
-        if (trigger.find('|', 0) != string::npos) {
-            trigger_rel = false;
-        }
-
-        if (action.find('|', 0) != string::npos) {
-            action_rel = false;
-        }
-
-        extractEqu(v[0], trigger, trigger_rel);
-        extractEqu(v[1], action, action_rel);
-       
-        return true;
-    }
-
-    void extractEqu(std::string peripheralcache, std::vector<Equation> &vec, bool rel){
-        boost::smatch what;
-        if (!boost::regex_match(peripheralcache, what, TARegEx)) {
-            getWarningsStream() << "match false"
-                                << "\n";
-            exit(0);
-            return false;
-        }
-
-        if (what.size() == 0) {
-            getWarningsStream() << "wrong size = " << what.size() << "\n";
-            exit(0);
-            return false;
-        }
-        for (auto equ: what) {
-            std::vector<std::string> v;
-            SplitString(what, v, ",");
-            Equation equ;
-            equ.rel = rel;
-            if (v[0] == "*") {
-                equ.type = v[0];
-            } else {
-                Equation equ;
-                equ.type = v[0];
-                equ.phaddr = std::stoull(v[1].c_str(), NULL, 10);
-                equ.bits = v[2];
-                equ.eq = v[3];
-                if (v[4][0] != '*') {
-                    equ.linkaddr = NULL;
-                    equ.value = std::stoull(v[4].c_str(), NULL, 10);
-                } else {
-                    equ.value = NULL;
-                    //std::stoull(v[4].substr(2, v[4].length()-2).c_str(), NULL, 10)
-                    if (v[4][1] == 'T')
-                        equ.linkaddr = &peripheral_regs_value_map[data_register].t_size;
-                    else if (v[4][1] == 'R')
-                        equ.linkaddr = &peripheral_regs_value_map[data_register].r_size;
-                }
-            }
-            vec.push_back(equ);
-        }
-    }
-
     void UpdateGraph(uint32_t type, uint32_t phaddr) {
         for (auto ta: allTAs) {
             std::vector<Equation> trigger = ta.first;
@@ -287,11 +139,15 @@ private:
 
 public:
     NLPPeripheralModelState() {
-        ReadMemofromFile('memory.txt');
-        ReadTAfromFile('register.txt');
     }
 
     virtual ~NLPPeripheralModelState() {
+    }
+
+    void initialize_graph(std::map<uint32_t, PeripheralReg>& m, std::vector<pari<std::vector<Equation>, std::vector<Equation>>> &ta, uint32_t dr) {
+        peripheral_regs_value_map = m;
+        allTAs = ta;
+        data_register = dr;
     }
 
     static PluginState *factory(Plugin *, S2EExecutionState *) {
@@ -332,14 +188,167 @@ public:
 };
 
 void NLPPeripheralModel::initialize() {
+    ReadMemofromFile('memory.txt');
+    ReadTAfromFile('register.txt');
 
     // bool ok;
     // ConfigFile *cfg = s2e()->getConfig();
-
+        
     hw::SymbolicPeripherals *symbolicPeripheralConnection = s2e()->getPlugin<hw::SymbolicPeripherals>();
     symbolicPeripheralConnection->onSymbolicNLPRegisterReadEvent.connect(sigc::mem_fun(*this, &NLPPeripheralModel::onPeripheralRead));
     symbolicPeripheralConnection->onSymbolicNLPRegisterWriteEvent.connect(sigc::mem_fun(*this, &NLPPeripheralModel::onPeripheralWrite));
 
+
+    DECLARE_PLUGINSTATE(NLPPeripheralModelState, state);
+    plgState->initialize_graph(peripheral_regs_value_map, allTAs, data_register);
+}
+
+bool NLPPeripheralModel::ReadMemofromFile(std::string fileName) {
+    std::ifstream fPHKB;
+    std::string line;
+    fPHKB.open(fileName, std::ios::in);
+    if (!fPHKB) {
+        getWarningsStream() << "Could not open cache peripheral knowledge base file: " 
+                            << fileName << " \n";
+        return false;
+    }
+
+    std::string peripheralcache;
+    while (getline(fPHKB, peripheralcache)) {
+        PeripheralReg reg;
+        if (getMemo(peripheralcache, &reg)) {
+            peripheral_regs_value_map[reg.phaddr] = reg;
+            if (reg.type == data_register_type)
+                data_register = reg.phaddr;
+        }
+    }
+    return true;
+}
+
+bool NLPPeripheralModel::ReadTAfromFile(std::string fileName) {
+    std::ifstream fPHKB;
+    std::string line;
+    fPHKB.open(fileName, std::ios::in);
+    if (!fPHKB) {
+        getWarningsStream() << "Could not open cache peripheral knowledge base file: " 
+                            << fileName << " \n";
+        return false;
+    }
+
+    std::string peripheralcache;
+    while (getline(fPHKB, peripheralcache)) {
+        std::vector<Equation> trigger;
+        std::vector<Equation> action;
+        if (getTApairs(peripheralcache, &trigger, &action)) {
+            allTAs.push_back(make_pair(trigger, action));
+        }
+    }
+    return true;
+}
+
+void NLPPeripheralModel::SplitString(const std::string &s, std::vector<std::string> &v, const std::string &c) {
+    std::string::size_type pos1, pos2;
+    pos2 = s.find(c);
+    pos1 = 0;
+    while (std::string::npos != pos2) {
+        v.push_back(s.substr(pos1, pos2 - pos1));
+
+        pos1 = pos2 + c.size();
+        pos2 = s.find(c, pos1);
+    }
+    if (pos1 != s.length())
+        v.push_back(s.substr(pos1));
+}
+
+bool NLPPeripheralModel::getMemo(std::string peripheralcache, PeripheralReg &reg) {
+    boost::smatch what;
+    if (!boost::regex_match(peripheralcache, what, MemoRegEx)) {
+        getWarningsStream() << "match false"
+                            << "\n";
+        exit(0);
+        return false;
+    }
+
+    if (what.size() != 1) {
+        getWarningsStream() << "wrong size = " << what.size() << "\n";
+        exit(0);
+        return false;
+    }
+
+    std::vector<std::string> v;
+    SplitString(what, v, "_");
+    reg.type = v[0];
+    reg.phaddr = std::stoull(v[1].c_str(), NULL, 10);
+    reg.reset = std::stoull(v[2].c_str(), NULL, 10);
+    reg.cur = reg.rest;
+    reg.t_size = 0;
+    reg.r_size = 0;
+    reg.t_value = 0;
+    reg.r_value = 0;
+    return true;
+}
+
+void NLPPeripheralModel::getTApairs(std::string peripheralcache, std::vector<Equation> &trigger, std::vector<Equation> &action) {
+    std::vector<std::string> v;
+    SplitString(peripheralcache, v, ":");
+    std::string trigger = v[0];
+    std::string action = v[1];
+    bool trigger_rel = true, action_rel = true;
+    if (trigger.find('|', 0) != string::npos) {
+        trigger_rel = false;
+    }
+
+    if (action.find('|', 0) != string::npos) {
+        action_rel = false;
+    }
+
+    extractEqu(v[0], trigger, trigger_rel);
+    extractEqu(v[1], action, action_rel);
+    
+    return true;
+}
+
+void NLPPeripheralModel::extractEqu(std::string peripheralcache, std::vector<Equation> &vec, bool rel){
+    boost::smatch what;
+    if (!boost::regex_match(peripheralcache, what, TARegEx)) {
+        getWarningsStream() << "match false"
+                            << "\n";
+        exit(0);
+        return false;
+    }
+
+    if (what.size() == 0) {
+        getWarningsStream() << "wrong size = " << what.size() << "\n";
+        exit(0);
+        return false;
+    }
+    for (auto equ: what) {
+        std::vector<std::string> v;
+        SplitString(what, v, ",");
+        Equation equ;
+        equ.rel = rel;
+        if (v[0] == "*") {
+            equ.type = v[0];
+        } else {
+            Equation equ;
+            equ.type = v[0];
+            equ.phaddr = std::stoull(v[1].c_str(), NULL, 10);
+            equ.bits = v[2];
+            equ.eq = v[3];
+            if (v[4][0] != '*') {
+                equ.linkaddr = NULL;
+                equ.value = std::stoull(v[4].c_str(), NULL, 10);
+            } else {
+                equ.value = NULL;
+                //std::stoull(v[4].substr(2, v[4].length()-2).c_str(), NULL, 10)
+                if (v[4][1] == 'T')
+                    equ.linkaddr = &peripheral_regs_value_map[data_register].t_size;
+                else if (v[4][1] == 'R')
+                    equ.linkaddr = &peripheral_regs_value_map[data_register].r_size;
+            }
+        }
+        vec.push_back(equ);
+    }
 }
 
 void NLPPeripheralModel::onPeripheralRead(S2EExecutionState *state, SymbolicHardwareAccessType type,
--- libs2eplugins/src/s2e/Plugins/NLP/NLPPeripheralModel.h
+++ libs2eplugins/src/s2e/Plugins/NLP/NLPPeripheralModel.h
@@ -27,6 +27,18 @@ public:
     sigc::signal<void, S2EExecutionState *, uint32_t /* irq_no */> onExternalInterruptEvent;
 
 private:
+    std::map<uint32_t, PeripheralReg> peripheral_regs_value_map;
+    std::vector<pari<std::vector<Equation>, std::vector<Equation>>> allTAs;
+    uint32_t data_register;
+    std::string data_register_type = 'R';
+    bool ReadMemofromFile(std::string fileName);
+    bool ReadTAfromFile(std::string fileName);
+    void SplitString(const std::string &s, std::vector<std::string> &v, const std::string &c);
+    bool getMemo(std::string peripheralcache, PeripheralReg &reg);
+    void getTApairs(std::string peripheralcache, std::vector<Equation> &trigger, std::vector<Equation> &action);
+    void extractEqu(std::string peripheralcache, std::vector<Equation> &vec, bool rel);
+
+
     sigc::connection symbolicPeripheralConnection;
     void onPeripheralRead(S2EExecutionState *state, SymbolicHardwareAccessType type, uint32_t phaddr,
                      unsigned size, uint32_t *NLPsymbolicvalue);
