0be8880fbdf94a7d673477ffc850496b210b1c4b
weizhou-chaojixx <weizhoulightning@gmail.com>
2021-09-25 23:23:44 +0800

    SymbolicPeripheral: concrete dr and stable regs
    Signed-off-by: weizhou-chaojixx <weizhoulightning@gmail.com>

--- libs2eplugins/src/s2e/Plugins/NLP/SymbolicPeripherals.cpp
+++ libs2eplugins/src/s2e/Plugins/NLP/SymbolicPeripherals.cpp
@@ -1090,6 +1090,13 @@ klee::ref<klee::Expr> SymbolicPeripherals::onNLPLearningMode(S2EExecutionState *
     getInfoStream(g_s2e_state) << ss.str() << " size " << hexval(size)
                                 << " SYM NLP value = " << hexval(NLP_value) << "\n";
 
+    uint64_t LSB = ((uint64_t) 1 << (size * 8));
+    if (address == 0x40005410 ||
+        (plgState->get_readphs_count(address) > 300 && !state->regs()->getInterruptFlag())) {
+        getInfoStream() << "return concrete value phaddr = " << hexval(address) << "\n";
+        uint32_t value = NLP_value & (LSB - 1);
+        return klee::ConstantExpr::create(value, size * 8);
+    }
     ConcreteArray concolicValue;
     SymbHwGetConcolicVector(NLP_value, size, concolicValue);
     return state->createSymbolicValue(ss.str(), size * 8, concolicValue);
